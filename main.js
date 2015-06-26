31728f6ddecf0553d6d38cb893d08b993ea2ac5b
AshishChoureCT <ashishchoure231989@gmail.com>
2015-06-26 15:28:52 +0530

    Small Fix for Routing Example

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ var module = angular.module("sampleApp", ['ngRoute']);
         function($routeProvider) {
             $routeProvider.
                 when('/route1', {
-                    templateUrl: '/template1.html',
+                    templateUrl: 'template1.html',
                     controller: 'RouteController1'
                 }).
                 when('/route2', {
-                    templateUrl: '/template2.html',
+                    templateUrl: 'template2.html',
                     controller: 'RouteController2'
                 }).
                 otherwise({
@@ -16,22 +16,22 @@ var module = angular.module("sampleApp", ['ngRoute']);
                 });
         }]);
 
-    module.controller("RouteController1", function ($scope, $q) {
-        var deferred = $q.defer();
-        $scope.name = "Ashish Choure";
-        test = function (username) {
-            setTimeout(function () {
-                return deferred.reject(username);
-            }, 500);
-            return deferred.promise;
-        };
-        var promise = test('TestCTUser');
-        promise.then(function (test) {
-            debugger;
-        }, function (error) {
-            debugger;
+        module.controller("RouteController1", function ($scope, $q) {
+            var deferred = $q.defer();
+            $scope.name = "Ashish Choure";
+            test = function (username) {
+                setTimeout(function () {
+                    return deferred.reject(username);
+                }, 500);
+                return deferred.promise;
+            };
+            var promise = test('TestCTUser');
+            promise.then(function (test) {
+                //Handle when deffered is successfully resolved.
+            }, function (error) {
+                console.log(error);
+            });
         });
-    });
     module.controller("RouteController2", function ($scope) {
         $scope.name = "Sumit Anand";
     });
